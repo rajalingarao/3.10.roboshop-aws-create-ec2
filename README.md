8711046c7f146711ef64e22c53fe20bee1a2c504
rajalingarao4545 <rajalingarao4545@gmail.com>
2026-05-20 23:48:03 +0530

    roboshop aws create ec2

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # 3.10.roboshop-aws-create-ec2
+
+
+```
+aws configure
+```
+access key:
+secret access key:
+region:
---
+++ main.tf
@@ -0,0 +1,27 @@
+resource "aws_instance" "roboshop_aws_create_ec2" {
+    ami           = data.aws_ami.rhel_info.id
+    instance_type = var.ec2_instance.instance_type
+    vpc_security_group_ids = [var.allow_everything]
+
+    # Define the root volume size and type
+    root_block_device {
+        encrypted             = false
+        volume_type           = "gp3"
+        volume_size           = 50
+        iops                  = 3000
+        throughput            = 80
+        delete_on_termination = true
+    }
+
+    tags = {
+        Name = "roboshop-aws-create-ec2"
+    }
+}
+resource "aws_route53_record" "roboshop_ec2_r53" {
+    zone_id = var.zone_id
+    name    = "roboshop_ec2.${var.domain_name}"
+    type    = "A"
+    ttl     = 1
+    records = [aws_instance.roboshop_aws_create_ec2.public_ip]
+    allow_overwrite = true
+}
